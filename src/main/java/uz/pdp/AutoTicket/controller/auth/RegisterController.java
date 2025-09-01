ccba4b44a61df7098555e1a42688e1111f3b6030
Odil1034 <odiljonbaxriddinov735@gmail.com>
2025-09-01 22:47:06 +0500

    jwt token utils added.

--- src/main/java/uz/pdp/AutoTicket/controller/auth/RegisterController.java
+++ src/main/java/uz/pdp/AutoTicket/controller/auth/RegisterController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class RegisterController {
 
+
     private final TokenService tokenService;
     private final UserService userService;
 
