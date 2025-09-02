ff2fbc26a00e8f7561663e813b5bd7de08c25819
Odil1034 <odiljonbaxriddinov735@gmail.com>
2025-09-02 23:18:02 +0500

    Write DTOs, Services

---
+++ src/main/java/uz/pdp/AutoTicket/controller/user/UserController.java
@@ -0,0 +1,7 @@
+package uz.pdp.AutoTicket.controller.user;
+
+/**
+* @author Baxriddinov Odiljon
+* @since 02/09/2025 21:33
+*/public class UserController {
+}
---
+++ src/main/java/uz/pdp/AutoTicket/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package uz.pdp.AutoTicket.repository;
+
+/**
+* @author Baxriddinov Odiljon
+* @since 02/09/2025 21:47
+*/public class AddressRepository {
+}
